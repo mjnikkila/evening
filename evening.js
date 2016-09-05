7a8ab854e6e61ca6fc4c3e04c2461891885c7b05
Mika Nikkilä <mika@slm.fi>
2016-09-05 23:10:45 +0300

    Evening core

---
+++ evening.js
@@ -0,0 +1,35 @@
+require("backbone-relational");
+require("backbone.epoxy");
+
+var evening_cache = {
+
+};
+
+module.exports = {
+    run: function(app) {
+        app();
+    },
+
+    Model: Backbone.Epoxy.Model.extend({
+
+    }),
+
+    Collection: Backbone.Collection.extend({
+
+    }),
+
+    View: Backbone.Epoxy.View.extend({
+        initialize: function() {
+            this.render();
+        },
+
+        render: function() {
+            var template = _.template(this.viewTemplate);
+            var attributes = {};
+            if(_.has(this, "model") && _.has(this.model, "attributes"))
+                attributes = this.model.attributes;
+            this.$el.html(template(attributes));
+            return this;
+        }
+    })
+};
